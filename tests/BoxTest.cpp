67d93451214ba440ad83816e4ed353ce6b0f850f
Stephan Vivie <stephan.vivie@student.hu.nl>
2016-03-16 23:37:10 +0100

    Writen the test methods Not implemented yet because underlying data types aren't complete as of now.

---
+++ tests/BoxTest.cpp
@@ -0,0 +1,116 @@
+#include "Box.hpp"
+
+
+/**
+* CONSTRUCTORS 
+*/
+
+TEST (Constructor, Coordinates) 
+{
+	// construct Box object with 2 Coordinates
+}
+
+TEST (Constructor, CoordinateDistance) 
+{
+	// construct Box object with Coordinate and Distance
+
+}
+
+/**
+* OPERATORS 
+*/
+
+TEST (Operators, Equal) 
+{
+
+}
+
+TEST (Operators, Ostream) 
+{
+
+}
+
+TEST (Operators, Istream) 
+{
+
+}
+
+/**
+* METHODS 
+*/
+
+TEST (Operators, ContainsCoordinate) 
+{
+	// test coordinate thats inside box
+
+	// test coordinate thats outside box
+
+}
+
+
+TEST (Operators, ContainsBox) 
+{
+	// test box thats inside box
+
+	// test box thats outside box
+
+	// test boxes that collide
+	
+}
+
+TEST (Operators, IntersectsBox) 
+{
+	// test box that is intersects
+
+	// test box that doesn't intersects
+}
+
+
+TEST (Operators, setBottomLeft) 
+{
+	// test if it sets the bottom left coordinate
+}
+
+TEST (Operators, getBottomLeft) 
+{
+	// test if it returns the right bottom left coordinate
+}
+
+
+
+TEST (Operators, setTopRight) 
+{
+	// test if it sets the top right coordinate
+
+}
+
+TEST (Operators, getTopRight) 
+{
+	// test if it returns the right top right coordinate
+}
+
+TEST (Operators, getUnionBox) 
+{
+	// test if method returns right result
+
+}
+
+TEST (Operators, getIntersectionBox) 
+{
+	// test if 2 boxes that are intersecting returns the right box
+
+	// test 2 boxes that aren't intersecting
+
+
+}
+
+TEST (Operators, setAxisSize) 
+{
+	// test is axis size sets the right length
+}
+
+TEST (Operators, getAxisSize) 
+{
+	// test if axis size returns the right length
+}
+
