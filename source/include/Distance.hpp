4a498cbe8415efb8bef3be2f3fdd80c86f05e214
RemcoDaniel <remco.nijkamp@student.hu.nl>
2016-03-31 10:08:57 +0200

    moved doxygen comments to right place (distance)

--- source/include/Distance.hpp
+++ source/include/Distance.hpp
@@ -18,42 +18,156 @@ class Coordinate;	// Forward declaration. Used to declare as friend.
 class Distance {
 		friend class Coordinate;
 	public:		// moet eigenlijk friend ofzo worden....
+		/**********************************************************************************************//**
+		 * @fn		Distance::Distance(double x, double y, double z)
+		 *
+		 * @brief	Constructor of a Distance
+		 *
+		 * @param	x			The X coordinate of the distance
+		 * @param	y			The Y coordinate of the distance
+		 * @param	z			The Z coordinate of the distance
+		 **************************************************************************************************/
 		Distance(double x = 0.0, double y = 0.0, double z = 0.0);
 		
 	public:
-		/// gets the x value of the Distance and returns it
+		/**********************************************************************************************//**
+		 * @fn		double Distance::getX()
+		 *
+		 * @brief	Gets the x value of the Distance and returns it
+		 *
+		 * @return	double		The x coordinate of the distance
+		 **************************************************************************************************/
 		double getX();
-		/// gets the y value of the Distance and returns it
+		/**********************************************************************************************//**
+		 * @fn		double Distance::getY()
+		 *
+		 * @brief	Gets the y value of the Distance and returns it
+		 *
+		 * @return	double		The y coordinate of the distance
+		 **************************************************************************************************/
 		double getY();
-		/// gets the z value of the Distance and returns it
+		/**********************************************************************************************//**
+		 * @fn		double Distance::getZ()
+		 *
+		 * @brief	Gets the z value of the Distance and returns it
+		 *
+		 * @return	double		The z coordinate of the distance
+		 **************************************************************************************************/
 		double getZ();
-		/// gets the Length of the Distance and returns it
+		/**********************************************************************************************//**
+		 * @fn		double Distance::getLength()
+		 *
+		 * @brief	Gets the calculated Length of the Distance and returns it
+		 *
+		 * @return	double		The Length of the distance
+		 **************************************************************************************************/
 		double getLength() const;
 		
-		/// sets the x value of the Distance to the given value
+		/**********************************************************************************************//**
+		 * @fn		void Distance::setX(double x)
+		 *
+		 * @brief	Sets the x value of the Distance to the given value
+		 *
+		 * @param	x			The x coordinate of the distance
+		 **************************************************************************************************/
 		void setX(double x);
-		/// sets the y value of the Distance to the given value
+		/**********************************************************************************************//**
+		 * @fn		void Distance::setY(double y)
+		 *
+		 * @brief	Sets the y value of the Distance to the given value
+		 *
+		 * @param	y			The y coordinate of the distance
+		 **************************************************************************************************/
 		void setY(double y);
-		/// sets the z value of the Distance to the given value
+		/**********************************************************************************************//**
+		 * @fn		void Distance::setZ(double z)
+		 *
+		 * @brief	Sets the z value of the Distance to the given value
+		 *
+		 * @param	z			The z coordinate of the distance
+		 **************************************************************************************************/
 		void setZ(double z);
 	
-		/// assignment operator for a Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance& Distance::operator= (const Distance& rhs)
+		 *
+		 * @brief	Assignment operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one is being assigned to another Distance
+		 * @return	Distance&	Reference to the Distance on which the assignment operator was called
+		 **************************************************************************************************/
 		Distance& operator= (const Distance& rhs);
-		/// adds a Distance to a Distance and returns a Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance Distance::operator+ (const Distance& rhs) const
+		 *
+		 * @brief	Add operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one is being added to another Distance
+		 * @return	Distance	The Distance on which the add operator was called
+		 **************************************************************************************************/
 		Distance operator+ (const Distance& rhs) const;
-		/// substracts a Distance from a Distance and returns a Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance Distance::operator- (const Distance& rhs) const
+		 *
+		 * @brief	Substract operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one is being substracted from another Distance
+		 * @return	Distance	The Distance on which the substract operator was called
+		 **************************************************************************************************/
 		Distance operator- (const Distance& rhs) const;
-		/// multiplies a Distance by a number and returns a Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance Distance::operator* (double number) const
+		 *
+		 * @brief	Multiply operator of a Distance
+		 *
+		 * @param	number		The number by which the Distance is being multiplied
+		 * @return	Distance	The Distance on which the multiply operator was called
+		 **************************************************************************************************/
 		Distance operator* (double number) const;
-		/// divides a Distance by a number and returns a Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance Distance::operator/ (double number) const
+		 *
+		 * @brief	Division operator of a Distance
+		 *
+		 * @param	number		The number by which the Distance is being divided
+		 * @return	Distance	The Distance on which the division operator was called
+		 **************************************************************************************************/
 		Distance operator/ (double number) const;
-		/// adds a Distance to a Distance and returns this Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance& Distance::operator+= (const Distance& rhs)
+		 *
+		 * @brief	AddAssign operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one is being added to another Distance
+		 * @return	Distance&	Reference to the Distance on which the addAssignment operator was called
+		 **************************************************************************************************/	
 		Distance& operator+= (const Distance& rhs);
-		/// substracts a Distance from a Distance and returns this Distance
+		/**********************************************************************************************//**
+		 * @fn		Distance& Distance::operator-= (const Distance& rhs)
+		 *
+		 * @brief	SubstractAssign operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one is being substracted from another Distance
+		 * @return	Distance&	Reference to the Distance on which the substractAssignment operator was called
+		 **************************************************************************************************/	
 		Distance& operator-= (const Distance& rhs);
-		/// compares two Distances to each other and returns a boolean (true if 1st is greater than 2nd)
+		/**********************************************************************************************//**
+		 * @fn		bool Distance::operator> (const Distance& rhs) const
+		 *
+		 * @brief	GreaterThan operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one will be compared to another Distance
+		 * @return	bool		True if length is greater, False if not
+		 **************************************************************************************************/
 		bool operator> (const Distance& rhs) const;
-		/// compares two Distances to each other and returns a boolean (true if 1st is smaller than 2nd)
+		/**********************************************************************************************//**
+		 * @fn		bool Distance::operator< (const Distance& rhs) const
+		 *
+		 * @brief	LessThan operator of a Distance
+		 *
+		 * @param	rhs			The right hand sight Distance, this one will be compared to another Distance
+		 * @return	bool		True if length is smaller, False if not
+		 **************************************************************************************************/
 		bool operator< (const Distance& rhs) const;
 		
 	private:
@@ -61,9 +175,25 @@ class Distance {
 		FRIEND_TEST(Distance, DefaultConstructor);	// Friend classes (So tests can access private parts.)
 };
 
-/// appends a Distance to an ostream and returns an ostream
+/**********************************************************************************************//**
+ * @fn		std::ostream& Distance::operator<< (std::ostream& lhs, const Distance& rhs) const
+ *
+ * @brief	Output operator of a Distance
+ *
+ * @param	lhs			The left hand sight ostream, to this ostream the Distance will be added
+ * @param	rhs			The right hand sight Distance, this one will be added to the lhs ostream
+ * @return	ostream&	Reference to the tweaked lhs ostream
+ **************************************************************************************************/
 std::ostream& operator<< (std::ostream& lhs, Distance& rhs);
-/// adds an istream to a Distance
+/**********************************************************************************************//**
+ * @fn		Distance& Distance::operator>>(std::istream& lhs, Distance& rhs)
+ *
+ * @brief	Input operator of a Distance
+ *
+ * @param	input		The istream, from this istream the Distance will be adapted
+ * @param	rhs			The right hand sight Distance, this one will be adapted by the lhs istream
+ * @return	Distance&	Reference to rhs Distance which is adapted by information from the lhs
+ **************************************************************************************************/
 Distance& operator>>(std::istream& input, Distance& rhs);
 
 #endif
--- source/src/Distance.cpp
+++ source/src/Distance.cpp
@@ -1,105 +1,39 @@
 #include "../include/Distance.hpp"
 
-/**********************************************************************************************//**
- * @fn		Distance::Distance(double x, double y, double z)
- *
- * @brief	Constructor of a Distance
- *
- * @param	x			The X coordinate of the distance
- * @param	y			The Y coordinate of the distance
- * @param	z			The Z coordinate of the distance
- **************************************************************************************************/
 Distance::Distance(double x, double y, double z) :
 	x(x),
 	y(y),
 	z(z)
 {}
 
-/**********************************************************************************************//**
- * @fn		double Distance::getX()
- *
- * @brief	Gets the x value of the Distance
- *
- * @return	double		The x coordinate of the distance
- **************************************************************************************************/
 double Distance::getX() {
 	return x;
 }
 
-/**********************************************************************************************//**
- * @fn		double Distance::getY()
- *
- * @brief	Gets the y value of the Distance
- *
- * @return	double		The y coordinate of the distance
- **************************************************************************************************/
 double Distance::getY() {
 	return y;
 }
 
-/**********************************************************************************************//**
- * @fn		double Distance::getZ()
- *
- * @brief	Gets the z value of the Distance
- *
- * @return	double		The z coordinate of the distance
- **************************************************************************************************/
 double Distance::getZ() {
 	return z;
 }
 
-/**********************************************************************************************//**
- * @fn		double Distance::getLength()
- *
- * @brief	Gets the calculated Length of the Distance
- *
- * @return	double		The Length of the distance
- **************************************************************************************************/
 double Distance::getLength() const {
 	return sqrt((x*x) + (y*y) + (z*z));
 }
 
-/**********************************************************************************************//**
- * @fn		void Distance::setX(double x)
- *
- * @brief	Sets the x value of the Distance
- *
- * @param	x			The x coordinate of the distance
- **************************************************************************************************/
 void Distance::setX(double x) {
 	x = x;
 }
 
-/**********************************************************************************************//**
- * @fn		void Distance::setY(double y)
- *
- * @brief	Sets the y value of the Distance
- *
- * @param	y			The y coordinate of the distance
- **************************************************************************************************/
 void Distance::setY(double y) {
 	y = y;
 }
 
-/**********************************************************************************************//**
- * @fn		void Distance::setZ(double z)
- *
- * @brief	Sets the z value of the Distance
- *
- * @param	z			The z coordinate of the distance
- **************************************************************************************************/
 void Distance::setZ(double z) {
 	z = z;
 }
 
-/**********************************************************************************************//**
- * @fn		Distance& Distance::operator= (const Distance& rhs)
- *
- * @brief	Assignment operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one is being assigned to another Distance
- * @return	Distance&	Reference to the Distance on which the assignment operator was called
- **************************************************************************************************/
 Distance& Distance::operator= (const Distance& rhs){
 	x = rhs.x;
 	y = rhs.y;
@@ -107,14 +41,6 @@ Distance& Distance::operator= (const Distance& rhs){
 	return *this;
 }
 
-/**********************************************************************************************//**
- * @fn		Distance Distance::operator+ (const Distance& rhs) const
- *
- * @brief	Add operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one is being added to another Distance
- * @return	Distance	The Distance on which the add operator was called
- **************************************************************************************************/
 Distance Distance::operator+ (const Distance& rhs) const {
 	Distance temp(*this);
 	temp.x += rhs.x;
@@ -123,14 +49,6 @@ Distance Distance::operator+ (const Distance& rhs) const {
 	return temp;
 }
 
-/**********************************************************************************************//**
- * @fn		Distance Distance::operator- (const Distance& rhs) const
- *
- * @brief	Substract operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one is being substracted from another Distance
- * @return	Distance	The Distance on which the substract operator was called
- **************************************************************************************************/
 Distance Distance::operator- (const Distance& rhs) const{
 	Distance temp(*this);
 	temp.x -= rhs.x;
@@ -139,14 +57,6 @@ Distance Distance::operator- (const Distance& rhs) const{
 	return temp;
 }
 
-/**********************************************************************************************//**
- * @fn		Distance Distance::operator* (double number) const
- *
- * @brief	Multiply operator of a Distance
- *
- * @param	number		The number by which the Distance is being multiplied
- * @return	Distance	The Distance on which the multiply operator was called
- **************************************************************************************************/
 Distance Distance::operator* (double number) const{
 	Distance temp(*this);
 	temp.x = temp.x * number;
@@ -154,15 +64,7 @@ Distance Distance::operator* (double number) const{
 	temp.z = temp.z * number;
 	return temp;
 }
-
-/**********************************************************************************************//**
- * @fn		Distance Distance::operator/ (double number) const
- *
- * @brief	Division operator of a Distance
- *
- * @param	number		The number by which the Distance is being divided
- * @return	Distance	The Distance on which the division operator was called
- **************************************************************************************************/		
+		
 Distance Distance::operator/ (double number) const{
 	Distance temp(*this);
 	if(number == 0){
@@ -174,83 +76,33 @@ Distance Distance::operator/ (double number) const{
 	return temp;
 }
 	
-/**********************************************************************************************//**
- * @fn		Distance& Distance::operator+= (const Distance& rhs)
- *
- * @brief	AddAssign operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one is being added to another Distance
- * @return	Distance&	Reference to the Distance on which the addAssignment operator was called
- **************************************************************************************************/	
 Distance& Distance::operator+= (const Distance& rhs){
 	this->x += rhs.x;
 	this->y += rhs.y;
 	this->z += rhs.z;
 	return *this;
 }
-
-/**********************************************************************************************//**
- * @fn		Distance& Distance::operator-= (const Distance& rhs)
- *
- * @brief	SubstractAssign operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one is being substracted from another Distance
- * @return	Distance&	Reference to the Distance on which the substractAssignment operator was called
- **************************************************************************************************/		
+	
 Distance& Distance::operator-= (const Distance& rhs) {
 	x -= rhs.x;
 	y -= rhs.y;
 	z -= rhs.z;
 	return *this;
 }
-
-/**********************************************************************************************//**
- * @fn		bool Distance::operator> (const Distance& rhs) const
- *
- * @brief	GreaterThan operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one will be compared to another Distance
- * @return	bool		True if length is greater, False if not
- **************************************************************************************************/		
+		
 bool Distance::operator> (const Distance& rhs) const {
 	return getLength() > rhs.getLength();
 }
-	
-/**********************************************************************************************//**
- * @fn		bool Distance::operator< (const Distance& rhs) const
- *
- * @brief	LessThan operator of a Distance
- *
- * @param	rhs			The right hand sight Distance, this one will be compared to another Distance
- * @return	bool		True if length is smaller, False if not
- **************************************************************************************************/	
+		
 bool Distance::operator< (const Distance& rhs) const {
 	return getLength() < rhs.getLength();
 }
-
-/**********************************************************************************************//**
- * @fn		std::ostream& Distance::operator<< (std::ostream& lhs, const Distance& rhs) const
- *
- * @brief	Output operator of a Distance
- *
- * @param	lhs			The left hand sight ostream, to this ostream the Distance will be added
- * @param	rhs			The right hand sight Distance, this one will be added to the lhs ostream
- * @return	ostream&	Reference to the tweaked lhs ostream
- **************************************************************************************************/		
+		
 std::ostream& operator<< (std::ostream& lhs, Distance& rhs) {
 	lhs << "(" << rhs.getX() << "," << rhs.getY() << "," << rhs.getZ() << ")";
 	return lhs;
 }
 		
-/**********************************************************************************************//**
- * @fn		Distance& Distance::operator>>(std::istream& lhs, Distance& rhs)
- *
- * @brief	Input operator of a Distance
- *
- * @param	input			The istream, from this istream the Distance will be adapted
- * @param	rhs			The right hand sight Distance, this one will be adapted by the lhs istream
- * @return	Distance&	Reference to rhs Distance which is adapted by information from the lhs
- **************************************************************************************************/
 Distance& operator>>(std::istream& input, Distance& rhs) {		// WAT WIL JE HIER TERUGKRIJGEN?? een Distance lijkt me?
 	std::string prefix;
 	input >> std::ws >> prefix;
