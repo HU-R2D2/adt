d117c996514f63c48e1a84b8d8cb3258f65348c5
Josque <joost@wagensveld.eu>
2016-04-01 13:11:06 +0200

    Pre-master branch created

--- source/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-#include "../include/Coordinate.hpp"
-#include "../include/Distance.hpp"
-#include "../include/Box.hpp"
-
-int main() {
-	std::cout << "Hello world";
-}
