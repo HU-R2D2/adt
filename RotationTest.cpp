d481f0ab6f8b6007764d758a7e61bc0f87904d53
CasperTI <casper@brothom.nl>
2016-04-05 10:27:19 +0200

    Added header and used Radians in Gtest

--- RotationTest.cpp
+++ RotationTest.cpp
@@ -1,8 +1,45 @@
+////
+//  ██████╗  ██████╗ ██████╗  ██████╗ ██████╗ ███████╗███████╗ ██████╗██╗   ██╗███████╗
+//  ██╔══██╗██╔═══██╗██╔══██╗██╔═══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██║   ██║██╔════╝
+//  ██████╔╝██║   ██║██████╔╝██║   ██║██████╔╝█████╗  ███████╗██║     ██║   ██║█████╗  
+//  ██╔══██╗██║   ██║██╔══██╗██║   ██║██╔══██╗██╔══╝  ╚════██║██║     ██║   ██║██╔══╝  
+//  ██║  ██║╚██████╔╝██████╔╝╚██████╔╝██║  ██║███████╗███████║╚██████╗╚██████╔╝███████╗
+//  ╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝
+//                                                                                                                                          
+//
+// @file RotationTest.cpp
+// @date Created: 31-03-16
+// @version 1.1.0
+//
+// @author Casper Wolf
+//
+// @section LICENSE
+// License: newBSD
+//
+// Copyright © 2016, HU University of Applied Sciences Utrecht.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// - Neither the name of the HU University of Applied Sciences Utrecht nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+////
 #include "gtest/gtest.h"
 #include "../source/include/Rotation.hpp"
 #include <iostream>
 
-// constructors
+double pi = 3.14159265358979323846;
 
 TEST(RotationConstructor, Default){
 	Rotation a;
@@ -11,12 +48,11 @@ TEST(RotationConstructor, Default){
 
 TEST(RotationConstructor, ConstructorValue){
 	Rotation a;
-	a = 20.0 * Rotation::rad;
-	ASSERT_DOUBLE_EQ(a.get_Rotation(),20.0);
+	a = pi * Rotation::rad;
+	ASSERT_DOUBLE_EQ(a.get_Rotation(),pi);
 }
 
 TEST(RotationConversion, Radians_Degrees){
-	double pi = 3.14159265358979323846;
 	EXPECT_DOUBLE_EQ(pi,3.14159265358979323846) << "Is Pi equal to the assigned variable or is the double still considered unequal anyway";
 	Rotation a =  pi * Rotation::rad;
 	Rotation b = 180.0 * Rotation::deg;
@@ -25,77 +61,80 @@ TEST(RotationConversion, Radians_Degrees){
 
 TEST(RotationAssign, Assign){
 	Rotation a();
-	a = 10.0 * Rotation::rad;
+	a = (pi/2) * Rotation::rad;
 	Rotation b();
-	EXPECT_DOUBLE_EQ(10.0,a.get_Rotation());
+	EXPECT_DOUBLE_EQ((pi/2),a.get_Rotation());
 	EXPECT_DOUBLE_EQ(0.0,b.get_Rotation());
 	b = a;
-	ASSERT_DOUBLE_EQ(10.0,b.get_Rotation());
+	ASSERT_DOUBLE_EQ((pi/2),b.get_Rotation());
 }
 	
 TEST(RotationComparison, Smaller){
-	Rotation a = 5.0 * Rotation::rad;
-	Rotation b = 3.0 * Rotation::rad;
+	Rotation a = (pi/4) * Rotation::rad;
+	Rotation b = (pi/8) * Rotation::rad;
 	ASSERT_EQ(a < b, false);
 	ASSERT_EQ(b < a, true);
 }
 
 TEST(RotationComparison, Bigger){
-	Rotation a = 4.0 * Rotation::rad;
-	Rotation b = 6.0 * Rotation::rad;
+	Rotation a = (pi/16) * Rotation::rad;
+	Rotation b = (pi/4) * Rotation::rad;
 	ASSERT_EQ(a > b, false);
-	ASSERT_EQ(b > a, false);
+	ASSERT_EQ(b > a, true);
 }
 
 TEST(RotationAdding, singleOperator){
-	Rotation a = 5.0 * Rotation::rad;
-	Rotation b = 6.0 * Rotation::rad;
+	Rotation a = (pi/2) * Rotation::rad;
+	Rotation b = (pi/2) * Rotation::rad;
 	Rotation c = a + b;
-	ASSERT_DOUBLE_EQ(c.get_Rotation(),11.0);
+	ASSERT_DOUBLE_EQ(c.get_Rotation(),(pi));
 }
 
 TEST(RotationAdding, dualOperator){
-	Rotation a = 5.0 * Rotation::rad;
-	Rotation b = 6.0 * Rotation::rad;
+	Rotation a = (pi/4) * Rotation::rad;
+	Rotation b = (pi/4) * Rotation::rad;
 	a += b;
-	ASSERT_DOUBLE_EQ(a.get_Rotation(),11.0);
+	ASSERT_DOUBLE_EQ(a.get_Rotation(),(pi/2));
 }
 
 TEST(RotationSubstraction, singleOperator){
-	Rotation a = 5.0 * Rotation::rad;
-	Rotation b = 2.0 * Rotation::rad;
+	Rotation a = pi * Rotation::rad;
+	Rotation b = (pi/2) * Rotation::rad;
 	Rotation c = a - b;
-	ASSERT_DOUBLE_EQ(c.get_Rotation(),3.0);
+	ASSERT_DOUBLE_EQ(c.get_Rotation(),(pi/2));
 }
 
 TEST(RotationSubstraction, dualOperator){
-	Rotation a = 5.0 * Rotation::rad;
-	Rotation b = 2.0 * Rotation::rad;
+	Rotation a = (2 * pi) * Rotation::rad;
+	Rotation b = pi * Rotation::rad;
 	a -= b;
-	ASSERT_DOUBLE_EQ(c.get_Rotation(),3.0);
+	ASSERT_DOUBLE_EQ(c.get_Rotation(),pi);
 }
 
 TEST(RotationMultiplication, singleOperator){
-	Rotation a = 5.0 * Rotation::rad;
+	Rotation a = pi * Rotation::rad;
 	a = a * 2.0;
-	ASSERT_DOUBLE_EQ(a.get_Rotation(),10.0);
+	ASSERT_DOUBLE_EQ(a.get_Rotation(),(pi * 2));
 }
 
 TEST(RotationMultiplication, dualOperator){
-	Rotation a = 5.0 * Rotation::rad;
-	a *= 3.0;
-	ASSERT_DOUBLE_EQ(a.get_Rotation(),15.0);
+	Rotation a = (pi/4) * Rotation::rad;
+	a *= 4.0;
+	ASSERT_DOUBLE_EQ(a.get_Rotation(),pi);
 }
 
 TEST(RotationDivision, singleOperator){
-	Rotation a = 5.0 * Rotation::rad;
+	Rotation a = (pi*2) * Rotation::rad;
 	a = a / 2.0;
-	ASSERT_DOUBLE_EQ(a.get_Rotation(),10.0);
+	ASSERT_DOUBLE_EQ(a.get_Rotation(),pi);
 }
 
 TEST(RotationDivision, dualOperator){
-	Rotation a = 5.0 * Rotation::rad;
-	a *= 3.0;
-	ASSERT_DOUBLE_EQ(a.get_Rotation(),15.0);
+	Rotation a = pi * Rotation::rad;
+	a /= 2.0;
+	ASSERT_DOUBLE_EQ(a.get_Rotation(),(pi/2));
 }
 
+TEST(RotatationOutstream, rotation){
+	Rotation a = pi * Rotation::rad;
+}
