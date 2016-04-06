a85a56e43de132c3325af6cd66b76c1f1b8bca96
Stephan Vivie <stephan.vivie@studnet.hu.nl>
2016-04-06 15:50:40 +0200

    Fixed indentation in Box and Acceleration

--- source/include/Box.hpp
+++ source/include/Box.hpp
@@ -34,88 +34,88 @@
 #include "Coordinate.hpp"
 #include "Translation.hpp"
 #include <iostream>
-	
-//!	@author 		Stephan Vivie
-//! @date			15-03-2016
-//!	@version		0.1
-//!	@brief	        A 3D rectangular box in space, which is aligned to the 3 axises.
-	
+
+//! @author         Stephan Vivie
+//! @date           15-03-2016
+//! @version        0.1
+//! @brief          A 3D rectangular box in space, which is aligned to the 3 axises.
+
 
 class Box final {
-	friend class Translation;
+    friend class Translation;
 public:
     Box();
-	//! @brief Constructs a box with 2 coordinates, bottom left and top right of the 3D ractangle
+    //! @brief Constructs a box with 2 coordinates, bottom left and top right of the 3D ractangle
     //!
     //! @param bottomLeft
     //! @param topRight 
-	Box (Coordinate bottomLeft, Coordinate topRight);
+    Box (Coordinate bottomLeft, Coordinate topRight);
 
-	//! @brief Constructs a box with one coordinate and a Translation, bottom left coordinate and the Translation of the axises.
+    //! @brief Constructs a box with one coordinate and a Translation, bottom left coordinate and the Translation of the axises.
     //!
     //! @param origin bottom left coordinate of the rectangle
     //! @param dist the size of one edge (axis) 
-	Box (Coordinate origin, Translation dist);
+    Box (Coordinate origin, Translation dist);
 
 public:
-	//! @brief Checks whether box contains coordinate
+    //! @brief Checks whether box contains coordinate
     //!
     //! @param coord The coordinate to check
     //! @return boolean State if parameter is inside of this object 
-	bool contains (const Coordinate & coord) const;
+    bool contains (const Coordinate & coord) const;
 
-	//! @brief Check whether box contains another box
+    //! @brief Check whether box contains another box
     //!
     //! @param box 
-    //! @return boolean State if parameter is inside of this object	
-	bool contains (const Box & box) const;
+    //! @return boolean State if parameter is inside of this object 
+    bool contains (const Box & box) const;
 
-	//! @brief Check whether box intersects with another box, returns boolean
+    //! @brief Check whether box intersects with another box, returns boolean
     //!
     //! @param 
-    //! @return 	
+    //! @return     
     bool intersects (const Box & box) const;
 
-	//! @brief return bottom left coordinate
+    //! @brief return bottom left coordinate
     //!
     //! @return bottomLeft coordinate
-	Coordinate get_bottom_left() const;
+    Coordinate get_bottom_left() const;
 
-	//! @brief return the top right coordinate
+    //! @brief return the top right coordinate
     //!
     //! @return topright coordinate
-	Coordinate get_top_right() const;
+    Coordinate get_top_right() const;
 
-	//! @brief Returns the smallest possible box that contains both boxes
+    //! @brief Returns the smallest possible box that contains both boxes
     //!
     //! @param box the box to create an union box with
     //! @return Box the union box
-	Box get_union_box(const Box & box) const;
+    Box get_union_box(const Box & box) const;
 
-	//! @brief Returns box that contains the shared space (intersection area) between two boxes
+    //! @brief Returns box that contains the shared space (intersection area) between two boxes
     //! @note If there is no intersection between the boxes, a empty box will be returned
     //!
     //! @param box the box to create an shared space with
     //! @return Box The box that contains the shared space. When there is no overlapse in the two boxes a box with (0.0,0.0,0.0) is returned.
-	Box get_intersection_box(const Box & box) const;
+    Box get_intersection_box(const Box & box) const;
 
-	//! @brief Returns the size of the axis
+    //! @brief Returns the size of the axis
     //!
     //! @return Translation of the axis
-	Translation get_axis_size() const;
+    Translation get_axis_size() const;
 
-	//! @brief assignment operator for a box
+    //! @brief assignment operator for a box
     //!
     //! @param box the box to assign
     //! @return the object
-	Box & operator= ( const Box & );
+    Box & operator= ( const Box & );
 
-	//! @brief appends a box to an std::ostream and returns the ostream
+    //! @brief appends a box to an std::ostream and returns the ostream
     //!
     //! @param os the std::ostream to append to
     //! @param rhs Object to append
     //! @return std::ostream
-	friend std::ostream & operator<< (std::ostream & os, const Box & rhs);
+    friend std::ostream & operator<< (std::ostream & os, const Box & rhs);
 
     //! @brief read a box from std::istream and return the istream
     //!
@@ -125,9 +125,9 @@ public:
     friend std::istream &operator>>(std::istream &lhs, Box & rhs);
 
 private:
-	Coordinate bottomLeft;
-	Coordinate topRight;
+    Coordinate bottomLeft;
+    Coordinate topRight;
 };
 
-#endif	// _BOX_HPP
+#endif  // _BOX_HPP
 
