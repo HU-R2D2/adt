06c1d7d400bc22105b45d7a23e1870fae1526105
RemcoDaniel <remco.nijkamp@student.hu.nl>
2016-04-06 15:08:38 +0200

    changed all tabs into spaces

--- source/include/Length.hpp
+++ source/include/Length.hpp
@@ -37,14 +37,14 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // ++--++
 //!
-//! @author		Remco Nijkamp & Koen de Groot
-//! @date 		15-03-16
-//! @version 	1.0
-//! @brief		Length is a Length is a measurement of a size that is being
+//! @author     Remco Nijkamp & Koen de Groot
+//! @date       15-03-16
+//! @version    1.0
+//! @brief      Length is a Length is a measurement of a size that is being
 //!             represented by a single value. 
-//!				Length stands on its own, this means it has no such thing as
+//!             Length stands on its own, this means it has no such thing as
 //!             a zero point or a direction.
-//!				The only attribute Length has, is the Length itself.
+//!             The only attribute Length has, is the Length itself.
 //!
 
 #ifndef _LENGTH_HPP
@@ -64,45 +64,45 @@ class Length: public ADT_Base<Length>{
     friend class Speed;
 
     protected:
-        //! @fn		Length::Length(double l)
+        //! @fn     Length::Length(double l)
         //!
-        //! @brief	Constructor of a Length
+        //! @brief  Constructor of a Length
         //!
-        //! @param	l The length value
+        //! @param  l The length value
         Length(double length);
 	
     public:
         friend ADT_Base<Length>;
-        //! @fn 	Length::Length()
+        //! @fn     Length::Length()
         //!
-        //! @brief 	Defaut constructor of length
+        //! @brief  Defaut constructor of length
         Length();
 
         //! @fn	std::ostream& Length::operator<< (std:: ostream& os, 
         //!const Length& rhs)
         //!
-        //! @brief	appends a Length to an ostream and returns an ostream
+        //! @brief  appends a Length to an ostream and returns an ostream
         //!
-        //! @param	rhs 		the right hand side to compare 
-        //! @return	ostream		...
+        //! @param	rhs     the right hand side to compare 
+        //! @return	ostream ...
         friend std::ostream& operator<< (std:: ostream& os, const Length& rhs);
 
-        //! @fn		std::istream& Length::operator>>(std::istream& is, Length& rhs)
+        //! @fn     std::istream& Length::operator>>(std::istream& is, Length& rhs)
         //!
-        //! @brief	adds an istream to a Length
+        //! @brief  adds an istream to a Length
         //!
-        //! @param	is		...
-        //! @param	rhs		...
-        //! @return	istream		...
+        //! @param	is      ...
+        //! @param	rhs     ...
+        //! @return	istream ...
         friend std::istream& operator>>(std::istream& is, Length& rhs);
 
-        //! @fn		Length Length::operator* (double n, const Length& rhs)
+        //! @fn     Length Length::operator* (double n, const Length& rhs)
         //!
-        //! @brief	multiply a Length by another Length and returns a Length
+        //! @brief  multiply a Length by another Length and returns a Length
         //!
-        //! @param	n		...
-        //!	@param	rhs		...
-        //! @return	Length		The result of the multiplication
+        //! @param	n       ...
+        //!	@param	rhs     ...
+        //! @return	Length  The result of the multiplication
         friend Length operator* (double n, const Length& rhs);
 
         //! @fn     Speed Length::operator/ (const Length & length, 
@@ -110,9 +110,9 @@ class Length: public ADT_Base<Length>{
         //!
         //! @brief  divide length by duration and return the speed
         //!
-        //! @param  length		...
-        //!	@param  d			...
-        //! @return Speed		The result of the division
+        //! @param  length      ...
+        //!	@param  d           ...
+        //! @return Speed       The result of the division
         friend Speed operator/ (const Length & length, const Duration & d);
 
         static const Length METER;
