42f5c00dfd0604af667503089dba6f0e287ba385
RemcoDaniel <remco.nijkamp@student.hu.nl>
2016-05-22 11:32:18 +0200

    added docygen documentation to attitude

--- source/include/Attitude.hpp
+++ source/include/Attitude.hpp
@@ -45,58 +45,100 @@ namespace r2d2{
 class Attitude{
     private:
         //constexpr double M_PI = 3.14159265358979323846;
-
         //! using x = pitch, y = yaw, z = roll
         Angle x,y,z;
 
-        //! positive value describing possible deviation from middle in either direction
+        //! positive value describing possible deviation
+        //! from middle in either direction
         const double angle_amp {M_PI};
 
-        //! set to 0 to have the values range from -pi to pi, or to pi to have them range from 0 to 2 * pi
+        //! set to 0 to have the values range from -pi to pi,
+        //! or to pi to have them range from 0 to 2 * pi
         const double middle_angle {M_PI};
 
     public:
+        //! @fn     Attitude::Attitude()
+        //! @brief  Default constructor of an Attitude
         Attitude();
     
-        //!@brief 
-        //!@param 
+        //! @fn     Attitude::Attitude(Angle pitch, Angle yaw, Angle roll)
+        //! @brief  Constructor of a Attitude
+        //! @param  pitch   The X Angle aka pitch of the Attitude
+        //! @param  yaw	    The Y Angle aka yaw of the Attitude
+        //! @param  roll	The Z Angle aka roll of the Attitude
         Attitude(Angle pitch, Angle yaw, Angle roll);
         
+        //! @fn     Angle Attitude::get_x()
+        //! @brief  Gets the x Angle aka pitch of the Attitude and returns it
+        //! @return	Angle	The x Angle aka pitch of the Attitude
         Angle get_x() const;
+        
+        //! @fn     Angle Attitude::get_y()
+        //! @brief  Gets the y Angle aka pitch of the Attitude and returns it
+        //! @return	Angle	The y Angle aka pitch of the Attitude
         Angle get_y() const;
+        
+        //! @fn     Angle Attitude::get_z()
+        //! @brief  Gets the z Angle aka pitch of the Attitude and returns it
+        //! @return	Angle	The z Angle aka pitch of the Attitude
         Angle get_z() const;
 
-        //!@brief Assignment operator for an Attitude
-        //!@param rhs Value to be copied into this Attitude
-        Attitude& operator=(const Attitude &rhs);
+        //! @fn     Attitude& Attitude::operator=(const Attitude& rhs)
+        //! @brief  Assignment operator of an Attitude
+        //! @param  rhs     The right hand sight Attitude, this one is 
+        //!                 being assigned to another Attitude
+        //! @return	Attitude& Reference to the Attitude on which the 
+        //!                      assignment operator was called
+        Attitude& operator=(const Attitude& rhs);
         
-        //!@brief
-        //!@param
-        Delta_Attitude operator+(const Attitude &rhs) const;
+        //! @fn Delta_Attitude Attitude::operator+(const Attitude& rhs) const
+        //! @brief  Add operator of an Attitude and another Attitude
+        //! @param  rhs    The Attitude that will be added to an Attitude
+        //! @return	Delta_Attitude    The Delta_Attitude that is created
+        //!                           by the addition
+        Delta_Attitude operator+(const Attitude& rhs) const;
         
-        //!@brief
-        //!@param
-        Delta_Attitude operator-(const Attitude &rhs) const;
+        //! @fn Delta_Attitude Attitude::operator-(const Attitude& rhs) const
+        //! @brief  Subtract operator of an Attitude and another Attitude
+        //! @param  rhs    The Attitude that will be subtracted from an Attitude
+        //! @return	Delta_Attitude    The Delta_Attitude that is created
+        //!                           by the subtraction
+        Delta_Attitude operator-(const Attitude& rhs) const;
         
-        //!@brief
-        //!@param
-        Attitude operator+(const Delta_Attitude &rhs) const;
+        //! @fn Attitude Attitude::operator+(const Delta_Attitude& rhs) const
+        //! @brief  Add operator of an Attitude and a Delta_Attitude
+        //! @param  rhs    The Delta_Attitude that will be added to an Attitude
+        //! @return	Attitude    The new Attitude that is created by the addition
+        Attitude operator+(const Delta_Attitude& rhs) const;
         
-        //!@brief
-        //!@param
-        Attitude operator-(const Delta_Attitude &rhs) const;
+        //! @fn Attitude Attitude::operator-(const Delta_Attitude& rhs) const
+        //! @brief  Subtract operator of an Attitude and a Delta_Attitude
+        //! @param  rhs     The Delta_Attitude that will be subtracted
+        //!                 from an Attitude
+        //! @return	Attitude The new Attitude that is created by the subtraction
+        Attitude operator-(const Delta_Attitude& rhs) const;
+        
+        //! @fn    Attitude& Attitude::operator+=(const Delta_Attitude& rhs)
+        //! @brief  AddAssign operator of an Attitude and a Delta_Attitude
+        //! @param  rhs     The Delta_Attitude that will be added to the Attitude
+        //! @return	Attitude& Reference to the Attitude on which the
+        //!                   AddAssign operator was called
+        Attitude& operator+=(const Delta_Attitude& rhs);
 
-        //!@brief Adds a Delta Attitude to this Attitude and returns this Attitude
-        //!@param
-        //!@return Attitude returns the changed Attitude
-        Attitude& operator+=(const Delta_Attitude &rhs);
+        //! @fn    Attitude& Attitude::operator-=(const Delta_Attitude& rhs)
+        //! @brief  SubtractAssign operator of an Attitude and a Delta_Attitude
+        //! @param  rhs       The Delta_Attitude that will be subtracted
+        //!                   from the Attitude
+        //! @return	Attitude& Reference to the Attitude on which the
+        //!                   SubtractAssign operator was called
+        Attitude& operator-=(const Delta_Attitude& rhs);
 
-        //!@brief
-        //!@param
-        Attitude& operator-=(const Delta_Attitude &rhs);
-
-        //!@brief
-        //!@param
+        //! @fn     std::ostream& Attitude::operator<<(std::ostream& lhs,
+        //!                       const Attitude& rhs)
+        //! @brief  Output operator of an Attitude
+        //! @param  lhs     The ostream to which the Attitude will be added
+        //! @param  rhs     The Attitude that will be added to the ostream
+        //! @return	ostream& Reference to the tweaked lhs ostream
         friend std::ostream& operator<<(std::ostream& lhs ,const Attitude& rhs);
 };
 }
